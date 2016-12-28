51277b4330699b314eb4edb4150ed1874cd8586e
dmereu <info@davidemereu.com>
2016-12-28 12:04:36 +0100

    Flood protection

--- addr2cap.py
+++ addr2cap.py
@@ -1,13 +1,15 @@
 # -*- coding: ISO-8859-1 -*-
 
-'''
+"""
 This script perform a massive search in Google Maps Database.
-The input is a string that contains a generic or incomplete address.
-The output is an array with address components, as stored in Google DB.
+Input: a string that contains a generic or incomplete address.
+Output: an array with address components, as stored in Google DB.
 Input and output is in standard csv file format, compatible with excel.
 The input address may be splitted into multiple columns.
-Due to flood protection until the "take a breath" system is not coded, take 200 requests a time.
-'''
+Flood protection with the "take a breath" system.
+It's possible to use the script in CLI mode, using the following parameters schema:
+addr2cap.py <input_file> <search_column_1> <search_column_2> <search_column_n>
+"""
 
 import time
 import sys
@@ -18,13 +20,13 @@ import logging
 import urllib.request
 
 log = True
-language = "it"
-logfile = "log.csv"
-csv_value_separator = ";"
-default_params = ["NULL", "input.csv", "Indirizzo Corrispondenza", "Città Corrispondenza"]
-requests_delay = 0
-take_a_breath_count = 30  # Avoid flood blocks grouping requests by n
-take_a_breath_pause = 10  # Pause between groups
+language = 'it'
+logfile = 'log.csv'
+csv_value_separator = ';'
+default_params = ['', 'input.csv', 'Indirizzo Corrispondenza', 'Città Corrispondenza']
+requests_delay = 1  # Seconds between requests
+take_a_breath_count = 100  # Avoid flood blocks grouping requests by n
+take_a_breath_pause = 10  # Seconds between groups
 requests_count = 0
 
 try:
@@ -34,14 +36,11 @@ try:
 except PermissionError:
     exit('Process aborted: the log file %s is locked by another application' % logfile)
 
-'''
-It's possible to use the script in CLI mode, using the following parameters schema:
-addr2cap.py <input_file> <search_column_1> <search_column_2> <search_column_n>
-'''
+
 if len(sys.argv) < 3:
-    logging.warning("Missing argouments. Usage: addr2cap <input_file> <search_column_1> <search_column_n>")
+    logging.warning('Missing argouments. Usage: addr2cap <input_file> <search_column_1> <search_column_n>')
     params = default_params
-    logging.warning("Using default parameters | Input file: %s | Search string list: %s" % (params[1], params[2:]))
+    logging.warning('Using default parameters | Input file: %s | Search string list: %s' % (params[1], params[2:]))
 else:
     params = sys.argv
 
@@ -49,27 +48,27 @@ infile = params[1]
 search_string_headers_list = params[2:]
 
 if not os.path.isfile(infile):
-    logging.error("Fatal error: input file '%s' not found \n ---" % infile)
+    logging.error('Fatal error: input file "%s" not found \n ---' % infile)
     exit()
 
 
 def cap_search(address_string):
     logging.info('Searching address details for "%s"' % address_string)
     url = 'http://maps.googleapis.com/maps/api/geocode/json?language=%s&address=%s' % \
-          (language, str.replace(address_string, " ", "+"))
+          (language, str.replace(address_string, ' ', '+'))
     try:
         r = urllib.request.urlopen(url)
         global requests_count
         requests_count += 1
     except urllib.request.HTTPError as err:
-        logging.debug("Error retriving data: %s" % err.code)
+        logging.debug('Error retriving data: %s' % err.code)
         return err.code
 
     data = json.loads(r.read().decode(r.info().get_param('charset') or 'ISO-8859-1'))
 
     if len(data['results']) > 0:
 
-        street = number = postal_code = city = state = "NULL"
+        street = number = postal_code = city = state = 'NULL'
 
         for field in data['results'][0]['address_components']:
 
@@ -84,12 +83,12 @@ def cap_search(address_string):
             elif 'administrative_area_level_2' in field['types']:
                 state = str(field['short_name']).upper()
 
-        if number == "NULL" and street != "NULL":
-            number = "SNC"
+        if number == 'NULL' and street != 'NULL':
+            number = 'SNC'
 
-        result = {"STREET": street, "NUMBER": number, "POSTAL_CODE": postal_code, "CITY": city, "STATE": state}
+        result = {'STREET': street, 'NUMBER': number, 'POSTAL_CODE': postal_code, 'CITY': city, 'STATE': state}
 
-        logging.info("Found: %s " % result)
+        logging.info('Found: %s ' % result)
     else:
         result = {}
 
@@ -97,45 +96,45 @@ def cap_search(address_string):
 
 
 def check_header_list(headers_list, file):
-    file_in_chk = open(file, 'r', encoding="ISO-8859-1")
+    file_in_chk = open(file, 'r', encoding='ISO-8859-1')
     file_reader = csv.DictReader(file_in_chk, delimiter=csv_value_separator)
     file_headers_list = file_reader.fieldnames
-    logging.debug("Searching for %s in headers..." % headers_list)
+    logging.debug('Searching for %s in headers...' % headers_list)
     for header in headers_list:
-        logging.debug("Checking %s..." % header)
+        logging.debug('Checking %s...' % header)
         if header in file_headers_list:
-            logging.debug("OK")
+            logging.debug('OK')
         else:
             return False
     return True
 
 
 if check_header_list(search_string_headers_list, infile):
-    logging.info("Column(s) with address string correctly founded in the input file's headers")
+    logging.info('Column(s) with address string correctly founded in the input file\'s headers')
 else:
-    logging.error("Fatal error: the column with search string was not found in the input file's headers")
+    logging.error('Fatal error: the column with search string was not found in the input file\'s headers')
     exit()
 
-f_in_chk = open(infile, 'r', encoding="ISO-8859-1")
+f_in_chk = open(infile, 'r', encoding='ISO-8859-1')
 reader = csv.DictReader(f_in_chk, delimiter=csv_value_separator)
 input_headers = reader.fieldnames
-logging.info("Checking input file. Headers founded: %s" % input_headers)
+logging.info('Checking input file. Headers founded: %s' % input_headers)
 rows_count = len(list(reader))
-logging.info("Found %s rows" % rows_count)
+logging.info('Found %s rows' % rows_count)
 
 
-f_in = open(infile, 'r', encoding="ISO-8859-1")
-outfile_fields = input_headers + ["STREET", "NUMBER", "POSTAL_CODE", "CITY", "STATE"]
-outfile = "%s_processed_%s.csv" % (infile[:-4], time.strftime("%Y%m%d%H%M", time.gmtime()))
+f_in = open(infile, 'r', encoding='ISO-8859-1')
+outfile_fields = input_headers + ['STREET', 'NUMBER', 'POSTAL_CODE', 'CITY', 'STATE']
+outfile = '%s_processed_%s.csv' % (infile[:-4], time.strftime('%Y%m%d%H%M', time.gmtime()))
 f_out = open(outfile, 'w')
 writer = csv.DictWriter(f_out, outfile_fields, lineterminator='\n', delimiter=';', dialect='excel', restval='')
-logfile = "activity_log.txt"
+logfile = 'activity_log.txt'
 
 dict_input = csv.DictReader(f_in, delimiter=csv_value_separator)
 dict_output = []
 writer.writeheader()
 
-logging.info("Process started...")
+logging.info('Process started...')
 
 count_ok = 0
 count_ko = 0
@@ -148,19 +147,25 @@ for row in dict_input:
         search_string_list.append(row[part])
     search_string = ' '.join(search_string_list)
 
+    # TAKE A BREATH
+    if requests_count > take_a_breath_count:
+        logging.info('Waiting %s seconds to avoid flood...' % take_a_breath_pause)
+        time.sleep(take_a_breath_pause)
+        logging.info('Ok, let's start again!')
+        requests_count = 0
+
     try:
         result = cap_search(search_string)
-
         if len(result) > 0:
             count_ok += 1
         else:
             count_ko += 1
-
-        print("Processing %s of %s | Found: %s | NOT Found %s |" % (dict_input.line_num - 1, rows_count, count_ok, count_ko))
+        print('Processing %s of %s | Found: %s | NOT Found %s |' % (dict_input.line_num - 1, rows_count, count_ok, count_ko))
         time.sleep(requests_delay)
     except (UnicodeEncodeError, UnicodeDecodeError, UnicodeDecodeError):
         logging.error('Error on string decoding. String skipped: %s' % row)
-        result = {"STREET": "NULL", "NUMBER": "NULL", "POSTAL_CODE": "NULL", "CITY": "NULL", "STATE": "NULL"}
+        result = {'STREET': 'NULL', 'NUMBER': 'NULL', 'POSTAL_CODE': 'NULL', 'CITY': 'NULL', 'STATE': 'NULL'}
+    requests_count += 1
 
     row.update(result)
     dict_output.append(row)
@@ -169,4 +174,4 @@ writer.writerows(dict_output)
 
 f_out.close()
 
-logging.info("Process ended. \n ---")
+logging.info('Process ended. \n ---')
